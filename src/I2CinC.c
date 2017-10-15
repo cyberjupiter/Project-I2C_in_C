937fe3ffeaaec854ef69f45a9a84d1a2b3cc5e13
Aiman Mazlan <aimanlpy@gmail.com>
2017-10-16 05:15:49 +0800

    Update I2CinC.c

--- src/I2CinC.c
+++ src/I2CinC.c
@@ -2,21 +2,30 @@
 
 #include <avr/io.h>
 #include <compat/twi.h>
+#include <inttypes.h>
 
 #include "Arduino.h"
 
 #include "I2CinC.h"
 
+/* define CPU frequency in hz here */
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+
+/* I2C clock in Hz */
+#define SCL_CLOCK  100000L
+
+
 void I2C_init(void)
 {
-	pinMode(SDA, OUTPUT);
-	pinMode(SCL, OUTPUT);
 	digitalWrite(SDA, 1);
 	digitalWrite(SCL, 1);
-	TWBR = 72;
+	TWSR = 0;
+	TWBR = ((F_CPU/SCL_CLOCK)-16)/2;
 }
 
-void I2C_start(void) // transmit address
+uint8_t I2C_start(void) // transmit address
 {
 	// reset TWI control register
 	TWCR = 0;
@@ -26,12 +35,58 @@ void I2C_start(void) // transmit address
 	while( !(TWCR & (1<<TWINT)) );
 	
 	// check if the start condition was successfully transmitted
-	if((TWSR & 0xF8) != TW_START){ return 1; }
+	if((TWSR & 0xF8) != TW_START)
+	{
+		return 1; 
+	}
 	
 	return 0;
 }
 
-void I2C_restart(void)
+
+/* thanks to Peter for this function */
+void I2C_start_wait(uint8_t address)
+{
+    uint8_t   twst;
+
+    while ( 1 )
+    {
+	    // send START condition
+	    TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+    
+    	// wait until transmission completed
+    	while(!(TWCR & (1<<TWINT)));
+    
+    	// check value of TWI Status Register. Mask prescaler bits.
+    	twst = TW_STATUS & 0xF8;
+    	if ( (twst != TW_START) && (twst != TW_REP_START)) continue;
+    
+    	// send device address
+    	TWDR = address;
+    	TWCR = (1<<TWINT) | (1<<TWEN);
+    
+    	// wail until transmission completed
+    	while(!(TWCR & (1<<TWINT)));
+    
+    	// check value of TWI Status Register. Mask prescaler bits.
+    	twst = TW_STATUS & 0xF8;
+    	if ( (twst == TW_MT_SLA_NACK )||(twst ==TW_MR_DATA_NACK) ) 
+    	{    	    
+    	    /* device busy, send stop condition to terminate write operation */
+	        TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+	        
+	        // wait until stop condition is executed and bus released
+	        while(TWCR & (1<<TWSTO));
+	        
+    	    continue;
+    	}
+    	//if( twst != TW_MT_SLA_ACK) return 1;
+    	break;
+     }
+
+}/* i2c_start_wait */
+
+uint8_t I2C_restart(void)
 { 
 	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
 	// wait for end of transmission
@@ -41,6 +96,7 @@ void I2C_restart(void)
 	
 	return 0;
 }
+
 
 uint8_t I2C_read_nack(void)
 {
@@ -60,7 +116,7 @@ uint8_t I2C_read_ack(void)
 	return TWDR;
 }
 
-uint8_t I2C_writeAddress(uint8_t address)
+uint8_t I2C_writeAddr(uint8_t address)
 {
 	// load data into data register
 	TWDR = address;
