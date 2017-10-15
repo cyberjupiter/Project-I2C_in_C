782d39db6ba48f688dbf4a7586019cffbd231ab0
Aiman Mazlan <aimanlpy@gmail.com>
2017-10-16 05:16:24 +0800

    Update I2CinC.h

--- src/I2CinC.h
+++ src/I2CinC.h
@@ -6,21 +6,25 @@ extern "C" {
 
 #ifndef I2CinC_h
 #define I2CinC_h
-	
-#define READ  0x01
-#define WRITE 0x00
+
+#include <inttypes.h>
+
+#define I2C_READ  0x01
+#define I2C_WRITE 0x00
 
 #define SDA	A4
-#define SCL	A5
+#define SCL A5
 
 void I2C_init(void);
-void I2C_start(void);
-void I2C_restart(void);
+uint8_t I2C_start(void);
+uint8_t I2C_restart(void);
 uint8_t I2C_writeData(uint8_t data);
-uint8_t I2C_writeAddress(uint8_t address);
+uint8_t I2C_writeAddr(uint8_t address);
+void I2C_start_wait(uint8_t address);
 uint8_t I2C_read_nack(void);
 uint8_t I2C_read_ack(void);
 void I2C_stop(void);
+uint8_t I2C_receiveData(uint8_t length);
 
 #endif
 
